3ba12ecc0228fa9cd1d0de7d7b6a9cf96a932458
Kevin Riehl <kevinriehl@gmail.com>
2016-03-08 10:36:50 -0800

    Added navbar

--- UrImg/UrImg.js
+++ UrImg/UrImg.js
@@ -1,7 +1,19 @@
+Router.route('/', function(){
+    this.render('hello');
+    this.layout('layout');  
+  });
+
 if (Meteor.isClient) {
   // counter starts at 0
-  Session.setDefault('counter', 0);
+  //Session.setDefault('counter', 0);
 
+  Accounts.ui.config({
+    //options are listed in book p. 135
+    //USERNAME_AND_EMAIL, USERNAME_AND_OPTIONAL_EMAIL
+    //USERNAME_ONLY, EMAIL_ONLY
+    passwordSignupFields: "USERNAME_AND_OPTIONAL_EMAIL"
+  });
+  
   Template.hello.helpers({
     counter: function () {
       return Session.get('counter');
